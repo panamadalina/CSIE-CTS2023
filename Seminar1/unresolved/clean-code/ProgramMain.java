beb2635bc28fcaa3171884df47ffc859b152d953
Madalina Pana <madalina.pana1997@gmail.com>
2023-02-20 00:14:02 +0200

    no message

---
+++ Seminar1/unresolved/clean-code/ProgramMain.java
@@ -0,0 +1,16 @@
+//package ....;
+
+public class ProgramMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        //Ex1: Det. number of occurrences of the minimum element from the vector:
+		Integer[] vector= {0, 2, 3, 4, -2, 0, 5, -2, 2};
+        // Use: Utils.java
+
+        //Ex2: Refactor code from EmployeeStudent.java
+
+	   //Ex3: Refactor code from classOne.java
+	}
+
+}
